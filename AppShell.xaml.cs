d628318ff6954e19ed559a3bb6bbac99663c4035
Sophia Dukhota (dukhotas) <dukhotas@coventry.ac.uk>
2023-03-09 19:04:06 +0000

    First commit with .NET MAUI project structure

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _6002CEM_SophiaDukhota;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _6002CEM_SophiaDukhota;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
