7dc70ba431a3732cb9b11080ac61cb7083f115bd
Sophia Dukhota (dukhotas) <dukhotas@coventry.ac.uk>
2023-04-24 22:08:15 +0100

    Display one item from recipeSearchView

--- Views/MainAppPage.xaml.cs
+++ Views/MainAppPage.xaml.cs
@@ -1,15 +1,23 @@
-﻿namespace _6002CEM_SophiaDukhota.Views;
+﻿using _6002CEM_SophiaDukhota.Models;
+
+namespace _6002CEM_SophiaDukhota.Views;
 
 public partial class MainAppPage : ContentPage
 {
 	public MainAppPage()
 	{
 		InitializeComponent();
-		List<string> plsWork = new List<string> {
-			"Xiao",
-			"Venti",
-			"Zhongli"
-		};
-		plsWorkView.ItemsSource = plsWork;
+		List<RecipesSearchModel> recipesSearchModel = new List<RecipesSearchModel>
+		{
+			new RecipesSearchModel { q="chicken", from=0, to=10, hits = new List<Hit>
+			{ new Hit{ recipe = new Recipe { uri = "someUri", label = "LABEL", externalId = "externalId"}},
+				new Hit { recipe = new Recipe { uri = "another URI", label = "L A B E L", externalId = "some stupid ID"} } } },
+
+            /* new RecipesSearchModel { q="beef", from=0, to=10, hits = new List<Hit>
+            { new Hit{ recipe = new Recipe { uri = "someUri2", label = "label2", externalId = "externalId2"} } } }*/
+
+        };
+
+        recipeSearchView.ItemsSource = recipesSearchModel;
     }
 }
